4765a552b3c3c6fa1ed481c278f2a720a80bb399
draconian56 <aydan.hardidge@connect.qut.edu.au>
2017-05-05 14:45:06 +1000

    The beta is ready for use as is. It needs more code comments, and a few more features yet to be added

--- DndDiceRoller/Form1.Designer.cs
+++ DndDiceRoller/Form1.Designer.cs
@@ -30,6 +30,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.calcButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // diceSides
@@ -42,16 +43,16 @@
             // 
             // outPutBox
             // 
-            this.outPutBox.Location = new System.Drawing.Point(621, 96);
+            this.outPutBox.Location = new System.Drawing.Point(773, 96);
             this.outPutBox.Multiline = true;
             this.outPutBox.Name = "outPutBox";
-            this.outPutBox.Size = new System.Drawing.Size(232, 223);
+            this.outPutBox.Size = new System.Drawing.Size(232, 358);
             this.outPutBox.TabIndex = 1;
             this.outPutBox.TextChanged += new System.EventHandler(this.outPutBox_TextChanged);
             // 
             // amountOfRolls
             // 
-            this.amountOfRolls.Location = new System.Drawing.Point(97, 288);
+            this.amountOfRolls.Location = new System.Drawing.Point(99, 243);
             this.amountOfRolls.Name = "amountOfRolls";
             this.amountOfRolls.Size = new System.Drawing.Size(197, 31);
             this.amountOfRolls.TabIndex = 2;
@@ -71,7 +72,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(92, 230);
+            this.label2.Location = new System.Drawing.Point(92, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(253, 37);
             this.label2.TabIndex = 4;
@@ -81,7 +82,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(621, 50);
+            this.label3.Location = new System.Drawing.Point(766, 50);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(122, 37);
             this.label3.TabIndex = 5;
@@ -90,7 +91,7 @@
             // calcButton
             // 
             this.calcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.calcButton.Location = new System.Drawing.Point(391, 165);
+            this.calcButton.Location = new System.Drawing.Point(450, 131);
             this.calcButton.Name = "calcButton";
             this.calcButton.Size = new System.Drawing.Size(171, 48);
             this.calcButton.TabIndex = 6;
@@ -98,11 +99,22 @@
             this.calcButton.UseVisualStyleBackColor = true;
             this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
             // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(773, 483);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(221, 47);
+            this.quitButton.TabIndex = 7;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1017, 553);
+            this.Controls.Add(this.quitButton);
             this.Controls.Add(this.calcButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -126,6 +138,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button quitButton;
     }
 }
 
--- DndDiceRoller/Form1.cs
+++ DndDiceRoller/Form1.cs
@@ -27,7 +27,49 @@ namespace DndDiceRoller {
         }
         // tells the program to start calculations
         private void calcButton_Click(object sender, EventArgs e) {
+            // Initial values for use later
+            int diceRolled;
+            int diceHasSides;
 
+            // Reset the output for the next amount of code
+            outPutBox.Text = "";
+
+            // Checking to see if both the inputs
+            // are actual integers, and not random
+            // characters.
+            if(!int.TryParse(diceSides.Text, out diceHasSides)) {
+                MessageBox.Show("Are you sure that's a number?");
+                diceSides.Text = "";
+                diceSides.Focus();
+                return;
+            }
+
+            if (!int.TryParse(amountOfRolls.Text, out diceRolled)) {
+                MessageBox.Show("Are you sure that's a number?");
+                amountOfRolls.Text = "";
+                amountOfRolls.Focus();
+                return;
+            }
+
+            int total = 0;
+            Random rnd = new Random();
+
+            for (int i = 1; i <= diceRolled; i++) {
+                int numberofRolls = rnd.Next(1, diceHasSides + 1);
+                Output(numberofRolls);
+                total = total + numberofRolls;
+            }
+
+            Output(total);
+        }
+
+        void Output(int value) {
+            outPutBox.Text += value.ToString("F2") + Environment.NewLine;
+        }
+
+        private void quitButton_Click(object sender, EventArgs e) {
+            MessageBox.Show("Thanks for using");
+            Application.Exit();
         }
     }
 }
